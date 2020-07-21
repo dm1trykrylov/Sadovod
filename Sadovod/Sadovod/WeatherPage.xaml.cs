f9b1acd3d75fb5c677632e813601a3246c768097
Dmitry <dkrylov@ithse.ru>
2020-07-21 16:27:14 +0400

    Min/Max temperature added to the forecast

--- Sadovod/Sadovod/WeatherPage.xaml.cs
+++ Sadovod/Sadovod/WeatherPage.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
+using Newtonsoft.Json;
 using System.Numerics;
 using System.Text;
 using System.Threading.Tasks;
@@ -22,18 +23,22 @@ namespace Sadovod
             GetLocation();
         }
 
+        private string curURL = "http://api.openweathermap.org/data/2.5/weather?";
+        private string forecastURL = "http://api.openweathermap.org/data/2.5/forecast?";
+        private const string AppID = "e02725c9ed5a4d87cc279b3dba21e6a6";
+        private Location location = new Location();
+
         private async void GetLocation()
         {
             try
             {
                 var request = new GeolocationRequest(GeolocationAccuracy.Medium);
-                var location = await Geolocation.GetLocationAsync(request);
+                location = await Geolocation.GetLocationAsync(request);
 
                 if (location != null)
-                {   
-                    //string URL = "api.openweathermap.org/data/2.5/weather?lat={" + (int)location.Latitude + "}&lon={" + 
-                    // (int)location.Longitude + "}&appid={e02725c9ed5a4d87cc279b3dba21e6a6}";
-                    //GetWeather(URL);
+                {
+                    var url = $"{curURL}lat={(int)location.Latitude}&lon={(int)location.Longitude}&appid={AppID}&units=metric";
+                    GetWeather(url);
                 }
             }
             catch (FeatureNotSupportedException fnsEx)
@@ -66,8 +71,14 @@ namespace Sadovod
                 if (response.IsSuccessStatusCode)
                 {
                     var result = await response.Content.ReadAsStringAsync();
-                    var info = Temp(result);
-                    await DisplayAlert("Hooray!", "Weather: " + info, "OK");
+                    var weatherInfo = JsonConvert.DeserializeObject<WeatherInfo>(result);
+                    currentTxt.Text = weatherInfo.weather[0].description.ToUpper();
+                    mainIconImg.Source = $"w{weatherInfo.weather[0].icon}";
+                    cityTxt.Text = weatherInfo.name.ToUpper();
+                    temperatureTxt.Text = weatherInfo.main.temp.ToString("0");
+
+                    DateTime dt = new DateTime().ToUniversalTime().AddSeconds(weatherInfo.dt);
+                    dateTxt.Text = dt.ToString("dddd, MMM dd").ToUpper();
                 }
             }
         }
@@ -76,6 +87,53 @@ namespace Sadovod
         {
             string novice = full.Substring(full.IndexOf("main"), full.IndexOf("description") - full.IndexOf("main"));
             return novice;
+        }
+
+        private async void GetForecast()
+        {
+            var url = $"{forecastURL}lat={(int)location.Latitude}&lon={(int)location.Longitude}&appid={AppID}&units=metric";
+
+            using (var client = new HttpClient())
+            {
+                var response = await client.PostAsync(url, null);
+                if (response.IsSuccessStatusCode)
+                {
+                    var result = await response.Content.ReadAsStringAsync();
+                    var forcastInfo = JsonConvert.DeserializeObject<ForecastInfo>(result);
+
+                    List<List> allList = new List<List>();
+
+                    foreach (var list in forcastInfo.list)
+                    {
+
+                        var date = DateTime.Parse(list.dt_txt);
+
+                        if (date > DateTime.Now && date.Hour == 0 && date.Minute == 0 && date.Second == 0)
+                            allList.Add(list);
+                    }
+                    dayOneTxt.Text = DateTime.Parse(allList[0].dt_txt).ToString("dddd");
+                    dateOneTxt.Text = DateTime.Parse(allList[0].dt_txt).ToString("dd MMM");
+                    iconOne.Source = $"w{allList[0].weather[0].icon}";
+                    maxTempOneTxt.Text = allList[0].main.temp_max.ToString("0");
+                    minTempOneTxt.Text = allList[0].main.temp_min.ToString("0");
+
+                    dayTwoTxt.Text = DateTime.Parse(allList[1].dt_txt).ToString("dddd");
+                    dateTwoTxt.Text = DateTime.Parse(allList[1].dt_txt).ToString("dd MMM");
+                    iconTwo.Source = $"w{allList[1].weather[0].icon}";
+                    maxTempTwoTxt.Text = allList[1].main.temp_max.ToString("0");
+                    minTempTwoTxt.Text = allList[1].main.temp_min.ToString("0");
+
+                    dayThreeTxt.Text = DateTime.Parse(allList[2].dt_txt).ToString("dddd");
+                    dateThreeTxt.Text = DateTime.Parse(allList[2].dt_txt).ToString("dd MMM");
+                    iconThree.Source = $"w{allList[2].weather[0].icon}";
+                    maxTempThreeTxt.Text = allList[2].main.temp_max.ToString("0");
+                    minTempThreeTxt.Text = allList[2].main.temp_min.ToString("0");
+                }
+                else
+                {
+                    await DisplayAlert("Weather Info", "No forecast information found", "OK");
+                }
+            }
         }
     }
 }
