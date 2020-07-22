ea3cb64d49e76f6b06db3df13660bf30da246468
Dmitry Krylov <dkrylov@ithse.ru>
2020-07-22 13:54:27 +0000

    Update App.xaml.cs

--- Sadovod/Sadovod/App.xaml.cs
+++ Sadovod/Sadovod/App.xaml.cs
@@ -10,7 +10,7 @@ namespace Sadovod
         {
             InitializeComponent();
 
-            MainPage = new TabbedPage();
+            MainPage = new MainPage();
         }
 
         protected override void OnStart()
