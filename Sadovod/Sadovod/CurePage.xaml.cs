e0e8c7ea370b2afe57a5302b935820928379c715
dmitry krylov <dkrylov@ithse.ru>
2020-07-23 02:03:23 +0400

    Update CurPage

--- Sadovod/Sadovod/CurePage.xaml.cs
+++ Sadovod/Sadovod/CurePage.xaml.cs
@@ -1,9 +1,11 @@
 ﻿using System;
+using System.Net;
+using System.Net.Http;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Newtonsoft.Json;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -17,65 +19,72 @@ namespace Sadovod
         public string PlantName { get; set; }
         public string PlantIllness { get; set; }
 
+        private string symptURL = "https://sadovodazfunc.azurewebsites.net/api/GetSympthoms?";
+
         public CurePage()
         {
             InitializeComponent();
-        }
-
-        //private async void SearchPlantButtonPressed(object sender, EventArgs e)
-        //{
-        //    PlantName = searchPlant.Text.ToString();
-        //    SymptomList = GetSymptoms(searchPlant.Text);
-        //    if(SymptomList.Length > 0)
-        //    {
-        //        OKbutton.IsVisible = true;
-
-        //        //SymptomPicker.ItemsSource = SymptomList;
-        //        //SymptomPicker.IsVisible = true;
-        //        IsSelectedItem = new bool[SymptomList.Length];
-        //        IsSelectedItem.SetValue(false, 0, SymptomList.Length - 1);
-
-        //    }
-        //    else
-        //    {
-        //        await DisplayAlert("Plant Search", "Unknown plant", "OK");
-        //    }
-        //}
-
-        private string[] GetSymptoms(string plant)
-        {
-            var testList = new string[] { "sympt_1", "sympt_2", "sympt_3" };
             
-            return testList;
         }
 
-        private void SymptomSelected (object sender, ItemTappedEventArgs e)
+        private async void SearchPlant(object sender, EventArgs e)
         {
-            if(e.Item != null)
+            PlantName = PlantSearchBar.Text.ToString();
+            GetSymptoms(PlantSearchBar.Text);
+            if (SymptomList.Length > 0)
             {
-                if(IsSelectedItem[e.ItemIndex] == true)
-                {
-                    ((ListView)sender).SelectedItem = null;
-                    IsSelectedItem[e.ItemIndex] = false;
-                }
-                else
-                {
-                    IsSelectedItem[e.ItemIndex] = true;
-                }
+                //OKbutton.IsVisible = true;
+
+                //SymptomPicker.ItemsSource = SymptomList;
+                //SymptomPicker.IsVisible = true;
+                IsSelectedItem = new bool[SymptomList.Length];
+                IsSelectedItem.SetValue(false, 0, SymptomList.Length - 1);
+
+            }
+            else
+            {
+                await DisplayAlert("Plant Search", "Unknown plant", "OK");
             }
         }
 
-        //private void OnButtonClicked (object sender, System.EventArgs e)
-        //{
-        //    //SymptomPicker.IsVisible = false;
-        //    OKbutton.IsVisible = false;
-        //    PlantIllness = GetIllness(PlantName, IsSelectedItem);
-        //    IName.Text = PlantIllness;
-        //    IName.IsVisible = true;
+        private async void GetSymptoms(string plant)
+        {
+            var url = $"{symptURL}{plant}";
+            using(var client = new HttpClient())
+            {
+                var response = await client.GetAsync(url);
+                if (response.IsSuccessStatusCode)
+                {
+                    var result = await response.Content.ReadAsStringAsync();
+                    var symptList = JsonConvert.DeserializeObject<SymptomsList>(result);
+                    int count = 1;
+                    foreach(string sympt in symptList.Symptoms)
+                    {
+                        SymptomListTxt.Text += $"{count++} {sympt}"
+                    }
+                }
+                else
+                {
+                    await DisplayAlert("Plant search", "Unknown plant", "OK");
+                }
+            }
 
-        //    IArticle.Text = GetInfo(PlantIllness);
-        //    IArticle.IsVisible = true;
-        //}
+            var testList = new string[] { "sympt_1", "sympt_2", "sympt_3" };
+            
+            
+        }
+
+       
+
+        private void OnButtonClicked(object sender, System.EventArgs e)
+        {
+            //SymptomPicker.IsVisible = false;
+            
+            PlantIllness = GetIllness(PlantName, IsSelectedItem);
+            
+        }
+
+       
 
         private string GetIllness (string plant, bool[] symptMask)
         {
